54c98506f2b4a7752ad3a96f842b1a670c3d26b7
Melinte Florin <florin.melinte12@yahoo.com>
2025-05-13 16:47:54 +0300

    sql script to select tables for testing

---
+++ src/tables/raw/clear_tables.sql
@@ -0,0 +1,12 @@
+DELETE FROM raw.user_profile;
+DELETE FROM raw.activity_log;
+DELETE FROM raw.sleep_log;
+DELETE FROM raw.nutrition_log;
+DELETE FROM raw.goals_log;
+
+SELECT * FROM raw.user_profile;
+SELECT * FROM raw.nutrition_log;
+SELECT * FROM raw.goals_log;
+SELECT * FROM raw.activity_log;
+SELECT * FROM raw.sleep_log;
+
